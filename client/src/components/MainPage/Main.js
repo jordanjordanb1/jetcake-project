609cd789ab538abe11b4bd22059a3242b5ffeafc
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-01-27 22:22:35 -0500

    Fixed positioning of cards

--- client/src/components/MainPage/Main.js
+++ client/src/components/MainPage/Main.js
@@ -7,12 +7,12 @@ import Login from './Login/Login'
 import Register from './Register/Register'
 
 export default function Main() {
-    const [container, setContainer] = useState(2)
+    const [container, setContainer] = useState(2) // 0 for login container, 1 for register container TODO: Change to more understable way
 
     return (
         <Container>
             <Row className="mt-xs-2 mt-4">
-                <Col xs={{ span: 4, offset: 4 }}>
+                <Col xs={{ span: 10, offset: 1 }} md={{ span: 4, offset: 4 }}>
                     <Login container={container} setContainer={setContainer} />
                     <Register container={container} setContainer={setContainer} />
                 </Col>

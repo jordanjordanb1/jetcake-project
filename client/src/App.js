6973f61bc1177cedceb32e4874b88ba400655247
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-01-27 23:47:47 -0500

    Added redux-persist

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.scss';
 import { Provider } from 'react-redux'
-import { ConfigureStore } from './redux/ConfigureStore'
+import { store, persistor } from './redux/ConfigureStore'
+import { PersistGate } from 'redux-persist/integration/react'
 import Main from './components/MainPage/Main';
+import AuthGuard from './components/AuthGuard/AuthGuard';
+import Login from './components/Login/Login';
 
-const store = ConfigureStore()
 
 function App() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Main />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/">
+                            <Main />
+                        </Route>
+
+                        <AuthGuard to="/login">
+                            <Login />
+                        </AuthGuard>
+                    </Switch>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     );
 }
--- client/src/redux/ConfigureStore.js
+++ client/src/redux/ConfigureStore.js
@@ -1,10 +1,19 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage/session'
 import { isProd } from "../config";
 
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import { user } from "./reducers/user";
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const pReducer = persistReducer(persistConfig, combineReducers({ user }))
 
 export const ConfigureStore = () => {
     let middleware = [];
@@ -19,11 +28,14 @@ export const ConfigureStore = () => {
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
     const store = createStore(
-        combineReducers({ user }),
+        pReducer,
         composeEnhancers(applyMiddleware(...middleware))
     );
 
     return store;
 };
 
-export default ConfigureStore;
+const store = ConfigureStore(),
+    persistor = persistStore(store)
+
+export { persistor, store }

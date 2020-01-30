b5c52a20a0423cafdd9d19bf3f8d473037fe9ea3
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-01-29 21:47:39 -0500

    Finished AddressForm component

--- client/src/components/PostLogin/Address/AddressForm.js
+++ client/src/components/PostLogin/Address/AddressForm.js
@@ -13,11 +13,34 @@ import jwt_decode from 'jwt-decode'
 
 const AddressForm = ({ token, history }) => {
     const [formStatus, setFormStatus] = useState(),
-        [formMsg, setFormMsg] = useState()
+        [formMsg, setFormMsg] = useState(),
+        USStates = [
+            'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA',
+            'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA',
+            'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+            'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+            'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
+        ]
 
     const AddressSchema = Yup.object().shape({
-        questions: Yup.object().shape({
-
+        address: Yup.object().shape({
+            street: Yup.string()
+                .min(5, "Street address is too short")
+                .max(25, "Street address is too long")
+                .required("Street address is required"),
+            city: Yup.string()
+                .min(5, "City is too short")
+                .max(25, "City is too long")
+                .required("City is required"),
+            state: Yup.string()
+                .min(2, "State is invalid")
+                .max(2, "State is invalid")
+                .oneOf(USStates, "State is invalid")
+                .required("Please choose a state"),
+            zip: Yup.string()
+                .min(4, "Zip code is invalid")
+                .max(5, "Zip code is invalid")
+                .required("Zip code is required")
         }),
     })
 
@@ -26,21 +49,22 @@ const AddressForm = ({ token, history }) => {
         <Formik
             initialValues={{
                 address: {
-
+                    street: '',
+                    city: '',
+                    state: '',
+                    zip: ''
                 }
             }}
 
             validationSchema={AddressSchema}
 
             onSubmit={
-                async ({ address }, { setSubmitting }) => {
+                async ({ address: { street, city, state, zip } }, { setSubmitting }) => {
                     const { _id } = jwt_decode(token)
 
                     setSubmitting(true)
 
-                    await axios.put(`/users/${_id}`, { 'address': '' }, { headers: { 'Authorization': `Bearer ${token}` } }).then(({ data: { success } }) => {
-                        console.log(success)
-
+                    await axios.put(`/users/${_id}`, { address: `${street}|${city}|${state}|${zip}` }, { headers: { 'Authorization': `Bearer ${token}` } }).then(({ data: { success } }) => {
                         if (success) {
                             setFormStatus(true)
                             setFormMsg("Address updated")
@@ -68,7 +92,7 @@ const AddressForm = ({ token, history }) => {
                         <Col xs={{ span: 10, offset: 1 }}>
                             <hr />
                             <div className="text-center form-reason">
-                                <h3>Please add your address</h3>
+                                <h3>Please enter your address</h3>
                             </div>
                             <hr />
 
@@ -81,26 +105,48 @@ const AddressForm = ({ token, history }) => {
                             }
 
                             <FormGroup>
-                                <ErrorMessage name="questions.one.question">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+                                <ErrorMessage name="address.street">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
 
                                 <InputGroup className="user-check-inputs">
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text><i className="fas fa-question"></i></InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <Field className="form-control" disabled={isSubmitting} type="text" name="questions.one.question" placeholder="Please enter a question..." />
+                                    <Field className="form-control" disabled={isSubmitting} type="text" name="address.street" placeholder="Street Address" />
                                 </InputGroup>
                             </FormGroup>
+
 
                             <FormGroup>
-                                <ErrorMessage name="questions.one.answer">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+                                <ErrorMessage name="address.city">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
 
                                 <InputGroup className="user-check-inputs">
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text><i style={{ color: 'transparent' }} className="fas fa-at"></i></InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <Field className="form-control" disabled={isSubmitting} type="text" name="questions.one.answer" placeholder="Please enter your answer..." />
+                                    <Field className="form-control" disabled={isSubmitting} type="text" name="address.city" placeholder="City" />
                                 </InputGroup>
                             </FormGroup>
+
+                            <Row>
+                                <FormGroup as={Col}>
+                                    <ErrorMessage name="address.state">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+
+                                    <InputGroup className="user-check-inputs">
+                                        <Field as="select" className="form-control" disabled={isSubmitting} type="text" name="address.state" placeholder="State">
+                                            <option>State...</option>
+                                            {
+                                                USStates.map(state => {
+                                                    return (
+                                                        <option key={state} value={state}>{state}</option>
+                                                    )
+                                                })
+                                            }
+                                        </Field>
+                                    </InputGroup>
+                                </FormGroup>
+
+                                <FormGroup as={Col}>
+                                    <ErrorMessage name="address.zip">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+
+                                    <InputGroup className="user-check-inputs">
+                                        <Field className="form-control" disabled={isSubmitting} type="number" name="address.zip" placeholder="Zip Code" min="00000" max="99999" />
+                                    </InputGroup>
+                                </FormGroup>
+                            </Row>
                         </Col>
                     </Row>
 

bba4145dbbf9393579707f27e0fe7ad460e00b1c
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-01 17:23:21 -0500

    Created navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import { Link } from 'react-router-dom'
+
+export default function NavbarComponent() {
+    return (
+        <Navbar bg="light">
+            <Container>
+                <Nav>
+                    <Link className="nav-link" to="/dashboard"><i className="fas fa-home"></i> Home</Link>
+                    <Link className="nav-link" to="/settings"><i className="fas fa-cog"></i> Settings</Link>
+                </Nav>
+
+                <Nav className="ml-auto">
+                    <Link className="nav-link" to="/logout"><i className="fas fa-sign-out-alt"></i> Logout</Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
